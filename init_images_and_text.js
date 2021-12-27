9a26ab7256f4cfab99ac24ae6d75166c61483405
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-12-27 17:35:08 +0100

    added more css features

--- init_images_and_text.js
+++ init_images_and_text.js
@@ -7,44 +7,26 @@ document.getElementById('version_number').innerHTML = whole_string;
 
 
 var request1 = new XMLHttpRequest();
-request1.open("GET", "imgs/array_gallery.json", false);
+request1.open("GET", "imgs/plot_gallery.json", false);
 request1.send(null);
 var jsonData1 = JSON.parse(request1.responseText);
-document.writeln(' <h1>2D Arrays</h1>')
+document.writeln(' <h1>My Plots</h1>')
 for (let key of Object.keys(jsonData1)) {
-    document.write(`<img src='imgs/${key}' alt= '${jsonData1[key]}' onclick='myFunction(this);' class='image'> `);
+    document.write(`<img src='imgs/${key}' alt= '${jsonData1[key][0]}' onclick='myFunction(this);' class='image' style= '${jsonData1[key][1]}'>  `);
 }
 
 
 
 
-var request2 = new XMLHttpRequest();
-request2.open("GET", "imgs/structuring_elements.json", false);
-request2.send(null);
-var jsonData4 = JSON.parse(request2.responseText);
-document.writeln(' <h1>3D Structuring Elements</h1>')
-for (let key of Object.keys(jsonData4)) {
-    document.write(`<img src='imgs/${key}' alt= '${jsonData4[key]}' onclick='myFunction(this);' class='imagebig'> `);
-}
-
-
-// var request3 = new XMLHttpRequest();
-// request3.open("GET", "imgs/3d_morphology.json", false);
-// request3.send(null);
-// var jsonData4 = JSON.parse(request3.responseText);
-// document.writeln(' <h1>3D Morphology</h1>')
+// var request2 = new XMLHttpRequest();
+// request2.open("GET", "imgs/structuring_elements.json", false);
+// request2.send(null);
+// var jsonData4 = JSON.parse(request2.responseText);
+// document.writeln(' <h1>3D Structuring Elements</h1>')
 // for (let key of Object.keys(jsonData4)) {
-//     document.write(`<img src='imgs/${key}' alt= '${jsonData4[key]}' onclick='myFunction(this);' class='imagelarge'> `);
+//     document.write(`<img src='imgs/${key}' alt= '${jsonData4[key]}' onclick='myFunction(this);' class='imagebig'> `);
 // }
 
-var request4 = new XMLHttpRequest();
-request4.open("GET", "imgs/napari.json", false);
-request4.send(null);
-var jsonData4 = JSON.parse(request4.responseText);
-document.writeln(' <h1>Napari</h1>')
-for (let key of Object.keys(jsonData4)) {
-    document.write(`<img src='imgs/${key}' alt= '${jsonData4[key]}' onclick='myFunction(this);' class='imagebig'> `);
-}
 
 // add copy button to images
 function myFunction(imgs) {
