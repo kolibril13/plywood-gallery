39340f0b8fb80452563524047423985f2cabe71c
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-01-02 17:19:55 +0100

    fix highlight js call

--- init_images_and_text.js
+++ init_images_and_text.js
@@ -1,34 +1,37 @@
 // @ts-check
 
-var version_number = "v0.0.1"
-var whole_string = "version ".concat(version_number)
+let version_number = "v0.0.1"
+let whole_string = "version ".concat(version_number)
 
 document.getElementById('version_number').innerHTML = whole_string;
 
 
-var request1 = new XMLHttpRequest();
+let request1 = new XMLHttpRequest();
 request1.open("GET", "imgs/gallery_parameters.json", false);
 request1.send(null);
-var chapter_of_html = "NAME_OF_CHAPTER";
-var chapter_of_example
-var jsonData1 = JSON.parse(request1.responseText);
+let chapter_of_html = "NAME_OF_CHAPTER_XYZ";
+let chapter_of_example
+let jsonData1 = JSON.parse(request1.responseText);
 for (let key of Object.keys(jsonData1)) {
     // json: key: imagepath, values: 0 -> chapter, 1 -> code, 2-> style
-    var path_of_example = key
-    var chapter_of_example = jsonData1[key][0]
-    var codeblock_of_example = jsonData1[key][1]
-    var css_of_example = jsonData1[key][2]
+    let path_of_example = key
+    let chapter_of_example = jsonData1[key][0]
+    let codeblock_of_example = jsonData1[key][1]
+    let css_of_example = jsonData1[key][2]
 
     if (chapter_of_html != chapter_of_example) {
         chapter_of_html = chapter_of_example
         document.writeln(`<h1> ${chapter_of_html} </h1>`)
     }
-    document.write(`<img src='imgs/${path_of_example}' alt= '${codeblock_of_example}' onclick='myFunction(this);' class='gallery_entry' style= '${css_of_example}'>  `);
+    document.write(`<img src='imgs/${path_of_example}' data-code= '${codeblock_of_example}' onclick='myFunction(this);' class='gallery_entry' style= '${css_of_example}'>  `);
 }
 
 
 // add code field
-function myFunction(imgs) {
-    var name = imgs.alt;
-    document.getElementById("info_field").innerHTML = name 
-}
+// function myFunction(imgs) {
+//     let name = imgs.dataset.code;
+//     hljs.initHighlighting.called = false;
+
+//     hljs.initHighlighting();
+//     document.getElementById("info_field").innerHTML = name
+// }
