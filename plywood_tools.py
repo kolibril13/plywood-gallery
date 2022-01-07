f5dcf149c754f27f347244f6cf0f0084179ba680
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-01-07 10:23:11 +0100

    start writing chapter_manager

---
+++ plywood_tools.py
@@ -0,0 +1,36 @@
+import json
+import os
+import shutil
+import time
+from base64 import b64decode
+from io import BytesIO
+from IPython import get_ipython
+from IPython.core.magic import register_cell_magic
+
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import seaborn as sns
+
+def rmtree(f: Path):
+    if f.is_file():
+        f.unlink()
+    else:
+        for child in f.iterdir():
+            rmtree(child)
+        f.rmdir()
+
+# delete all previous files
+path = Path.cwd() / "gallery_assets"
+try:
+    rmtree(path)
+except:
+    pass
+
+path.mkdir(parents=False, exist_ok=False)
+
+# create json file
+joson_file_path = "gallery_assets/gallery_parameters.json"
+with open(joson_file_path, "w") as jsonFile:
+    json.dump({}, jsonFile, indent=2)
