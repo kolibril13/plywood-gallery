8985332400ed82364dd40d18c0aa01dd501c7e18
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-01-07 10:01:42 +0100

    adjust javascript

--- init_images_and_text.js
+++ init_images_and_text.js
@@ -1,12 +1,12 @@
 // @ts-check
 
-let version_number = "v0.0.1"
+let version_number = "v0.0.2"
 let whole_string = "version ".concat(version_number)
 
 document.getElementById('version_number').innerHTML = whole_string;
 
 let request1 = new XMLHttpRequest();
-request1.open("GET", "imgs/gallery_parameters.json", false);
+request1.open("GET", "gallery_assets/gallery_parameters.json", false);
 request1.send(null);
 
 let jsonData1 = JSON.parse(request1.responseText);
@@ -21,7 +21,6 @@ for (let key of Object.keys(jsonData1)) {
         let path_of_example = element_dict["image_path"]
         let codeblock_of_example = element_dict["code"]
         let css_of_example = element_dict["css"]
-        document.write(`<img src='imgs/${path_of_example}' data-code= '${codeblock_of_example}' onclick='display_code_from_gallery_cell(this);' class='gallery_entry' style= '${css_of_example}'>  `);
-
+        document.write(`<img src='${path_of_example}' data-code= '${codeblock_of_example}' onclick='display_code_from_gallery_cell(this);' class='gallery_entry' style= '${css_of_example}'>  `);
     }
 }
