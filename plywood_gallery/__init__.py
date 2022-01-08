11d8eb4b908c53d3ece9e52d5962c47103dae144
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-01-08 21:58:15 +0100

    add new debugging infrastructure

--- plywood_gallery/__init__.py
+++ plywood_gallery/__init__.py
@@ -1,5 +1,8 @@
 from .plywood_tools import  PlywoodGalleryMagic , ChapterManager
 
 from IPython import get_ipython # register cell magic
-ipy = get_ipython()
-ipy.register_magics(PlywoodGalleryMagic)
+try:
+    ipy = get_ipython()
+    ipy.register_magics(PlywoodGalleryMagic)
+except:
+    pass
---
+++ test.py
@@ -0,0 +1,3 @@
+from plywood_gallery.plywood_tools import ChapterManager
+z = ChapterManager.cell_counter
+print(z)
