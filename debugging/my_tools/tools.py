1583771d22e376566b5193145d5243acd6db2a46
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-01-08 20:05:37 +0100

    moving to module

--- debugging/my_tools/tools.py
+++ debugging/my_tools/tools.py
@@ -1,20 +1,18 @@
-# from IPython.core.magic import (Magics, magics_class, cell_magic)
-# from IPython import get_ipython
-# from IPython.utils.capture import capture_output
+from IPython.core.magic import (Magics, magics_class, cell_magic)
+from IPython.utils.capture import capture_output
 
-# @magics_class
-# class MyMagic(Magics):
-#     @cell_magic
-#     def caputre_png_test(self, line, cell):
-#         print("hello caputre_png_test")
-#         with capture_output(stdout=False, stderr=False, display=False) as result:
-#             for output in result.outputs:
-#                 data = output.data
-#                 print(data.keys())
+@magics_class
+class MyMagic(Magics):
+    @cell_magic
+    def caputre_png_test(self, line, cell):
+        print("hello caputre_png_test")
+        with capture_output(stdout=False, stderr=False, display=True) as result:
+            self.shell.run_cell(cell)
+        print(result.outputs)
 
-#     @cell_magic
-#     def another_new_cellmagic(self, line, cell):
-#         "my cell magic"
-#         print("Hello another_new_cellmagic")
+    @cell_magic
+    def another_new_cellmagic(self, line, cell):
+        "my cell magic"
+        print("Hello another_new_cellmagic")
 
 
