525bda4fb25ea869970b3e5e5aedd51e60ca9102
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-01-09 19:59:37 +0100

    add quickstart

--- plywood_gallery/quickstart.py
+++ plywood_gallery/quickstart.py
@@ -3,8 +3,13 @@ from pathlib import Path
 
 def quickstart():
 
-    src = Path(__file__).resolve() / "template" / "template_index.html"
-    dest = Path.cwd()
+    src = Path(__file__).resolve().parent / "template" / "template_index.html"
+    dest = Path.cwd() / "new_index.html"
+    print(src,dest)
+    #dest.write_bytes(src.read_bytes()) #for binary files # better this?
+    dest.write_text(src.read_text()) #for text files
 
-   # dest.write_bytes(src.read_bytes()) #for binary files
+    src = Path(__file__).resolve().parent / "template" / "template_gallery.ipynb"
+    dest = Path.cwd() / "new_gallery.ipynb"
+    print(src,dest)
     dest.write_text(src.read_text()) #for text files
