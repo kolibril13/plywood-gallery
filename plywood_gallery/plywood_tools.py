06fc384fa3943915ae30aba04264cbeca5961134
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-04-02 23:56:30 +0200

    black

--- plywood_gallery/plywood_tools.py
+++ plywood_gallery/plywood_tools.py
@@ -22,24 +22,29 @@ def rmtree(f: Path):
             rmtree(child)
         f.rmdir()
 
-class SilentServer(http.server.SimpleHTTPRequestHandler): #Opens the webserver and makes sure that there is no long log message
+
+class SilentServer(
+    http.server.SimpleHTTPRequestHandler
+):  # Opens the webserver and makes sure that there is no long log message
     protocol_version = "HTTP/1.0"
 
     def log_message(self, *args):
         pass
 
+
 class ChapterManager:
     """Recives instructions from  capture_png_test"""
+
     cell_counter = 0
     chapter_name = ""
-    path = Path.cwd() / "gallery_assets/" # cwd of folder where jupyter notebook is in
+    path = Path.cwd() / "gallery_assets/"  # cwd of folder where jupyter notebook is in
     json_path = Path.cwd() / "gallery_assets/gallery_parameters.json"
 
     @staticmethod
-    def open_webpage(PORT = 8000):
+    def open_webpage(PORT=8000):
         def thread_function():
             Handler = SilentServer
-            try: # make sure that server is not already running
+            try:  # make sure that server is not already running
                 with socketserver.TCPServer(("", PORT), Handler) as httpd:
                     print("serving at port", PORT)
                     httpd.serve_forever()
@@ -49,14 +54,16 @@ class ChapterManager:
         mythread = threading.Thread(target=thread_function)
         mythread.start()
 
-        url  = f"http://localhost:{PORT}/"
-        print(f"{url} will now be opened in your default browser. Closing this notebook will also shut the server")
+        url = f"http://localhost:{PORT}/"
+        print(
+            f"{url} will now be opened in your default browser. Closing this notebook will also shut the server"
+        )
         webbrowser.open_new_tab(url)
 
     @staticmethod
     def set_chapter_name(new_chapter):
         """Makes a new chapter"""
-        ChapterManager.chapter_name =  new_chapter    
+        ChapterManager.chapter_name = new_chapter
 
     @staticmethod
     def reset_counter():
@@ -65,11 +72,12 @@ class ChapterManager:
 
     @staticmethod
     def sort(chapter_order):
-        """ Sort chapters according to the list that is given.
+        """Sort chapters according to the list that is given.
         TODO: Make exception when name did not occur."""
 
         # sort chapter
         import json
+
         new_order = chapter_order
         joson_file_path = ChapterManager.json_path
         with open(joson_file_path, "r") as jsonFile:
@@ -82,7 +90,7 @@ class ChapterManager:
             data.pop(chapter_name)
 
         new_data = {}
-        for chapter_name , temp in zip(new_order, temp_data):
+        for chapter_name, temp in zip(new_order, temp_data):
             new_data[chapter_name] = temp
 
         new_data.update(data)
@@ -97,7 +105,7 @@ class ChapterManager:
             data = json.load(jsonFile)
 
         image_list = []
-        chapter_content= data[chapter_name]
+        chapter_content = data[chapter_name]
         for entry in chapter_content:
             image_list.append(entry["image_path"])
 
@@ -108,23 +116,25 @@ class ChapterManager:
                 whole_path.unlink()
             except:
                 pass
-            
+
         data.pop(chapter_name)
         with open(ChapterManager.json_path, "w") as jsonFile:
             json.dump(data, jsonFile, indent=2, sort_keys=False)
 
     @staticmethod
-    def clean_all(skip_warning= False):
+    def clean_all(skip_warning=False):
         """Cleans the whole gallery_assets tree. User will be asked to confirm the cleaning first
         After cleaning, a new json file will be created."""
-        print(f"This path and all its child elements will be removed:{ChapterManager.path}")
+        print(
+            f"This path and all its child elements will be removed:{ChapterManager.path}"
+        )
         if not skip_warning:
             if input("are you sure? (y/n)") != "y":
                 raise ValueError("Could not delete folder because no permission")
             else:
                 pass
 
-        path  = ChapterManager.path
+        path = ChapterManager.path
         try:
             rmtree(path)
         except:
@@ -135,13 +145,14 @@ class ChapterManager:
     @staticmethod
     def generate_json():
         """Creates a new empty json file for gallery information."""
-        path  = ChapterManager.path
+        path = ChapterManager.path
         path.mkdir(parents=False, exist_ok=True)
         # create json file
         joson_file_path = ChapterManager.json_path
         with open(joson_file_path, "w") as jsonFile:
             json.dump({}, jsonFile, indent=2)
-    
+
+
 @magics_class
 class PlywoodGalleryMagic(Magics):
     @magic_arguments.magic_arguments()
@@ -164,35 +175,35 @@ class PlywoodGalleryMagic(Magics):
         help=("Add extra css style for the gallery enteries"),
     )
     @cell_magic
-    def capture_png(self,line, cell):
+    def capture_png(self, line, cell):
         """Saves the png image and the css style for the html page"""
         args = magic_arguments.parse_argstring(PlywoodGalleryMagic.capture_png, line)
 
         postpath = args.path
-        chapter_name= ChapterManager.chapter_name
+        chapter_name = ChapterManager.chapter_name
         chapter_name_underscore = chapter_name.replace(" ", "_")
         joson_file_path = ChapterManager.json_path
-        prepath = ChapterManager.path.name # get the last child folder name
+        prepath = ChapterManager.path.name  # get the last child folder name
         ChapterManager.cell_counter += 1
         # include chaptername
         path = f"{prepath}/{chapter_name_underscore}_{ChapterManager.cell_counter:03}_{postpath}"
 
-        #path = path.split(".png")[0] + str(time.time_ns()) + ".png"
+        # path = path.split(".png")[0] + str(time.time_ns()) + ".png"
         if not path:
-            raise ValueError('No path found!')
+            raise ValueError("No path found!")
 
         style = args.style
         style = style.strip('"')  # remove quotes
 
         styles = {
-            'Normal': 'border: 3px solid #007AB8;',
-            'Header': 'border: 3px solid #ED6A5A;',
-            'Dependend': 'border: 3px solid #A8DCF0;'
+            "Normal": "border: 3px solid #007AB8;",
+            "Header": "border: 3px solid #ED6A5A;",
+            "Dependend": "border: 3px solid #A8DCF0;",
         }
         try:
             default_style = styles[args.celltype]
         except KeyError:
-            raise ValueError('Not a valid cell type!')
+            raise ValueError("Not a valid cell type!")
 
         style = default_style + style
 
@@ -231,29 +242,31 @@ class PlywoodGalleryMagic(Magics):
 
         chapter_content = data[chapter_name]
         chapter_content.append(
-            {"image_path": path,
-            "celltype": args.celltype,
-            "css": style,
-            "code": code_block})
+            {
+                "image_path": path,
+                "celltype": args.celltype,
+                "css": style,
+                "code": code_block,
+            }
+        )
 
         data[chapter_name] = chapter_content
         with open(joson_file_path, "w") as jsonFile:
             json.dump(data, jsonFile, indent=2, sort_keys=False)
 
-
         # save the output
         with capture_output(stdout=False, stderr=False, display=True) as result:
-            self.shell.run_cell(cell) # idea by @krassowski 
+            self.shell.run_cell(cell)  # idea by @krassowski
 
         # save image
         for output in result.outputs:
             display(output)
             data = output.data
-            if 'image/png' in data:
-                png_bytes = data['image/png']
+            if "image/png" in data:
+                png_bytes = data["image/png"]
                 if isinstance(png_bytes, str):
                     png_bytes = b64decode(png_bytes)
                 assert isinstance(png_bytes, bytes)
                 bytes_io = BytesIO(png_bytes)
                 image = PIL.Image.open(bytes_io)
-                image.save(path, 'png')
+                image.save(path, "png")
