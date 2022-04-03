229aa67a2f8266157224e2a12b49113e49fa648c
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-04-03 14:13:36 +0200

    devide template folders

--- plywood_gallery/load_jinja.py
+++ plywood_gallery/load_jinja.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 def load_jinja2_template():
-    templates_dir = Path(__file__).resolve().parent / "template"
+    templates_dir = Path(__file__).resolve().parent / "jinja2_template"
     env = Environment(loader=FileSystemLoader(templates_dir))
     template = env.get_template("template_index.html")
     return template
