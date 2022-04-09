e2ad53b425874797239bed8a3a0e65297094ed59
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-04-09 17:46:50 +0200

    fix test

--- tests/test_first_try.py
+++ tests/test_first_try.py
@@ -4,8 +4,8 @@ def fib(n: int) -> int:
     return n if n < 2 else fib(n-1)+fib(n-2)
 
 def test_fibonacci():
-    assert fib(10) == 54
-
+    assert fib(10) == 55
+    
 # from plywood_gallery import ChapterManager
 
 
