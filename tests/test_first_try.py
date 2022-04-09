9b2f5a7e33158f23a2a3d5dcfd04a4633576eea5
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-04-09 17:45:17 +0200

    fail test on purpose

--- tests/test_first_try.py
+++ tests/test_first_try.py
@@ -4,7 +4,7 @@ def fib(n: int) -> int:
     return n if n < 2 else fib(n-1)+fib(n-2)
 
 def test_fibonacci():
-    assert fib(10) == 55
+    assert fib(10) == 54
 
 # from plywood_gallery import ChapterManager
 
