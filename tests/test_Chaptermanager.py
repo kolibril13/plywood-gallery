4a3024f706244305689c29c3de8e110190faaf5f
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-04-20 10:19:12 +0200

    add ChapterManager.generate_json to test

--- tests/test_Chaptermanager.py
+++ tests/test_Chaptermanager.py
@@ -17,15 +17,15 @@ def test_chaptermanager_json_path():
     assert ChapterManager.json_path.name == "gallery_parameters.json"
     assert ChapterManager.json_path.parents[0].name == "gallery_assets"
 
-    """Create file and test existence"""
+    # """Create file and test existence"""
     ChapterManager.set_assets_folder_name("my_new_example_assets")
 
     assert ChapterManager.json_path.name == "gallery_parameters.json"
     assert ChapterManager.json_path.parents[0].name == "my_new_example_assets"
 
+    ChapterManager.generate_json()
     assert ChapterManager.json_path.exists() is True
 
     ChapterManager.clean_all(skip_warning=True)
 
     assert ChapterManager.json_path.exists() is False
-
